9d3733ee9d2916a1f139d57d56296e37acb8de17
Manuel <mannelpz25@gmail.com>
2022-07-14 13:56:19 -0500

    Sección 17 video 243

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,9 +1,54 @@
-import { Typography } from "@mui/material"
+import { Google } from "@mui/icons-material"
+import { Grid,Typography,TextField,Button,Link } from "@mui/material"
+import { Link as RouterLink } from "react-router-dom"
+import { AuthLayout } from "../layout/AuthLayout"
 
 export const RegisterPage = () => {
   return (
-    <>
-        <Typography variant='h1'>RegisterPage</Typography>
-    </>
+    <AuthLayout title="Register">
+        <form >
+          <Grid container>
+          <Grid item xs={12} sx={{mt: 2}}>
+              <TextField 
+                label="Nombre Completo" 
+                type="text" 
+                placeholder="John Rose"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sx={{mt: 2}}>
+              <TextField 
+                label="Correo" 
+                type="email" 
+                placeholder="correo@google.com"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sx={{mt: 2}}>
+              <TextField 
+                label="Contraseña" 
+                type="password" 
+                placeholder="Contraseña"
+                fullWidth
+              />
+            </Grid>
+            <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
+              <Grid item xs={12}>
+                <Button variant="contained" fullWidth>
+                  Crear cuenta    
+                </Button>
+              </Grid>
+              
+            </Grid>
+            <Grid container direction='row' justifyContent='end'>
+              <Typography sx={{mr:1}}>¿Ya tienes cuenta?</Typography>
+              <Link component={RouterLink} color='inherit' to="/auth/login">
+                Ingresar
+              </Link>             
+               
+            </Grid>
+          </Grid>
+        </form>
+    </AuthLayout>
   )
 }
